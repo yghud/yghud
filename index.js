c6bb1b34ceba1e67cb8ca882a12449ca240c2468
bigcerts <bigcerts@gmail.com>
2024-03-05 19:50:55 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const { ethers } = require('ethers');
+
+function createWallet() {
+    const wallet = ethers.Wallet.createRandom();
+    console.log(`Wallet Address: ${wallet.address}`);
+    console.log(`Mnemonic: ${wallet.mnemonic.phrase}`);
+    console.log(`Private Key: ${wallet.privateKey}`);
+}
+
+module.exports = { createWallet };
